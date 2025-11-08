94f2615222e46d6b4b7a4c3c73593f1e97687341
xziste <17alexdmz99@gmail.com>
2025-11-08 11:18:23 +0300

    update settings.py

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "users",
+    "reservation",
 ]
 
 MIDDLEWARE = [
@@ -131,3 +133,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'users.User'
