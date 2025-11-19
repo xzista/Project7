eb33d1c4df04cdfde6239d0779bde0bbf23f1471
xziste <17alexdmz99@gmail.com>
2025-11-19 12:26:13 +0300

    update settings.py

--- config/settings.py
+++ config/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv("POSTGRES_DB"),
+        "NAME": os.getenv("POSTGRES_NAME"),
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "HOST": os.getenv("POSTGRES_HOST", "db"),
