edaf9f665ec836b02712261a1842a920237eeb94
xziste <17alexdmz99@gmail.com>
2025-11-19 11:51:21 +0300

    update settings.py

--- config/settings.py
+++ config/settings.py
@@ -87,8 +87,8 @@ DATABASES = {
         "NAME": os.getenv("POSTGRES_DB"),
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": os.getenv("POSTGRES_HOST"),
-        "PORT": os.getenv("POSTGRES_PORT"),
+        "HOST": os.getenv("POSTGRES_HOST", "db"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
     }
 }
 
