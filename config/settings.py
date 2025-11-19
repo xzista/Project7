dbea107e012d051aa9a991d5ca4ce5d4a991440c
xziste <17alexdmz99@gmail.com>
2025-11-19 23:50:52 +0300

    fixed csrf in settings.py

--- config/settings.py
+++ config/settings.py
@@ -30,8 +30,13 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.getenv("DEBUG") == "True" else False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",") if os.getenv("ALLOWED_HOSTS") else ["*"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "http://84.252.132.229",
+    "http://localhost",
+    "http://127.0.0.1",
+]
 
 # Application definition
 
