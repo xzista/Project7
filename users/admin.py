edc5bc993836b17409aed597ac6f1d1ac87571d0
xziste <17alexdmz99@gmail.com>
2025-11-19 06:46:05 +0300

    update admin in users

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "email",
+        "phone",
+        "avatar",
+        "is_active",
+    )
+    list_filter = (
+        "email",
+        "is_active",
+    )
+    search_fields = ("email", "phone",)
