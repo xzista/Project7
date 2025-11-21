b8229e129abb03878b855b31f9cf707563f44913
xziste <17alexdmz99@gmail.com>
2025-11-21 06:12:11 +0300

    added admin panel in new apps

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import MenuCategory, Dish
 
-# Register your models here.
+@admin.register(MenuCategory)
+class MenuCategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "category", "price", "is_available")
+    list_filter = ("category", "is_available")
+    search_fields = ("name", "description")
--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("id", "first_name", "last_name", "role", "rating", "is_published", "created_at")
+    list_filter = ("role", "rating", "is_published")
+    search_fields = ("first_name", "last_name", "comment")
+    actions = ["make_published", "make_unpublished"]
+
+    def make_published(self, request, queryset):
+        updated = queryset.update(is_published=True)
+        self.message_user(request, f"{updated} отзыв(ов) опубликовано.")
+    make_published.short_description = "Опубликовать выбранные отзывы"
+
+    def make_unpublished(self, request, queryset):
+        updated = queryset.update(is_published=False)
+        self.message_user(request, f"{updated} отзыв(ов) снято с публикации.")
+    make_unpublished.short_description = "Снять публикацию"
