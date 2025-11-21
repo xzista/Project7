de41c6786f84d759f94c469c4a04cc42669e2e4e
xziste <17alexdmz99@gmail.com>
2025-11-21 06:12:38 +0300

    added views in new apps

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Dish, MenuCategory
+from django.db.models import Q
 
-# Create your views here.
+class DishListView(ListView):
+    model = Dish
+    template_name = "menu/dish_list.html"
+    context_object_name = "dishes"
+    paginate_by = 12
+
+    def get_queryset(self):
+        qs = Dish.objects.filter(is_available=True).select_related("category")
+        q = self.request.GET.get("q")
+        cat = self.request.GET.get("category")
+        price_min = self.request.GET.get("price_min")
+        price_max = self.request.GET.get("price_max")
+
+        if q:
+            qs = qs.filter(Q(name__icontains=q) | Q(description__icontains=q))
+        if cat:
+            qs = qs.filter(category__id=cat)
+        if price_min:
+            try:
+                qs = qs.filter(price__gte=float(price_min))
+            except ValueError:
+                pass
+        if price_max:
+            try:
+                qs = qs.filter(price__lte=float(price_max))
+            except ValueError:
+                pass
+        return qs.order_by("category__name", "name")
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx["categories"] = MenuCategory.objects.all()
+        ctx["q"] = self.request.GET.get("q", "")
+        ctx["selected_category"] = self.request.GET.get("category", "")
+        ctx["price_min"] = self.request.GET.get("price_min", "")
+        ctx["price_max"] = self.request.GET.get("price_max", "")
+        return ctx
+
+class DishDetailView(DetailView):
+    model = Dish
+    template_name = "menu/dish_detail.html"
+    context_object_name = "dish"
--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView
+from .models import Review
+from .forms import ReviewForm
+from django.urls import reverse_lazy
+from django.contrib import messages
 
-# Create your views here.
+class ReviewListView(ListView):
+    model = Review
+    template_name = "reviews/review_list.html"
+    context_object_name = "reviews"
+    paginate_by = 10
+
+    def get_queryset(self):
+        return Review.objects.filter(is_published=True).order_by("-created_at")
+
+class ReviewCreateView(CreateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = "reviews/review_form.html"
+    success_url = reverse_lazy("reviews:review_list")
+
+    def form_valid(self, form):
+        # по умолчанию отзывы требуют модерации: is_published=False
+        response = super().form_valid(form)
+        messages.success(self.request, "Спасибо! Ваш отзыв отправлен на модерацию.")
+        return response
